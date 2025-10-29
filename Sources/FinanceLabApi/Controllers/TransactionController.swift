75bb4ccea3aafbc11a50e8199988f5e84d837555
Anne Ferret <anne@anneferret.eu>
2025-10-29 15:20:23 +0100

    cleanup and comments in TransactionController

--- Sources/FinanceLabApi/Controllers/TransactionController.swift
+++ Sources/FinanceLabApi/Controllers/TransactionController.swift
@@ -27,90 +27,107 @@ struct TransactionController: RouteCollection {
 
     @Sendable
     func create(req: Request) async throws -> TransactionDTO {
+        // Authentificate user
         let payload = try req.auth.require(UserPayload.self)
+        
+        // Find user from token in the database
         guard let user = try await User.find(payload.id, on: req.db) else {
             throw Abort(.notFound, reason: "User not found")
         }
-        
+
+        // Initialize our transaction DTO
         let dto = try req.content.decode(TransactionDTO.self)
         
-        // Create the actual Transaction model
+        // Create the transaction model
         let transaction = Transaction()
         transaction.name = dto.name
         transaction.iconName = dto.iconName
         transaction.amount = dto.amount
         transaction.date = dto.date
         transaction.contractor = dto.contractor
-        transaction.$user.id = try user.requireID()
+        transaction.$user.id = try user.requireID()     // user ID from token
         
+        // Saves on database and return transaction as DTO
         try await transaction.save(on: req.db)
-        
         return transaction.toTransactionDTO()
     }
     
-    func getById(req: Request) async throws -> Transaction {
-        let payload = try req.auth.require(UserPayload.self)
-        guard let user = try await User.find(payload.id, on: req.db) else {
-            throw Abort(.notFound, reason: "User not found")
-        }
-        
+    func getById(req: Request) async throws -> TransactionDTO {
+        // Authentificate user
+        let _ = try req.auth.require(UserPayload.self)
+
+        // Finds transaction by ID
         guard let transaction = try await Transaction.find(req.parameters.get("transactionID"), on: req.db) else {
             throw Abort(.notFound, reason: "Transaction not found")
         }
-        transaction.$user.id = try user.requireID()
-        try await transaction.save(on: req.db)
         
-        return transaction
+        // Return transaction as DTO
+        return transaction.toTransactionDTO()
     }
 
     func delete(req: Request) async throws -> HTTPStatus {
-        let payload = try req.auth.require(UserPayload.self)
+        // Authentificate user
+        let _ = try req.auth.require(UserPayload.self)
         
-        guard let user = try await User.find(payload.id, on: req.db) else {
-            throw Abort(.notFound)
-        }
-        
+        // Finds transaction by ID
         guard let transaction = try await Transaction.find(req.parameters.get("transactionID"), on: req.db) else {
-            throw Abort(.notFound)
+            throw Abort(.notFound, reason: "Transaction not found")
         }
+        
+        // Delete transaction from database
         try await transaction.delete(on: req.db)
+        
+        // Returns http status confirmation without content
         return .noContent
     }
 
-    func update(req: Request) async throws -> Transaction {
+    func update(req: Request) async throws -> TransactionDTO {
+        // Authentificate user
+        let _ = try req.auth.require(UserPayload.self)
+
+        // Takes existing transaction from database
         guard let existing = try await Transaction.find(req.parameters.get("transactionID"), on: req.db) else {
             throw Abort(.notFound, reason: "Transaction not found")
         }
+        
+        // Takes DTO sent from client
         let dto = try req.content.decode(TransactionDTO.self)
-        // Preserve the existing identifier to ensure we update the correct record
+        
+        // Values to update
         existing.name = dto.name
         existing.iconName = dto.iconName
         existing.amount = dto.amount
         existing.date = dto.date
         existing.contractor = dto.contractor
 
+        // Saves on database and returns updated transaction as DTO
         try await existing.save(on: req.db)
-        return existing
+        return existing.toTransactionDTO()
     }
     
     func getByUserID(req: Request) async throws -> [TransactionDTO] {
+        // Authentificate user
         let payload = try req.auth.require(UserPayload.self)
         
+        // Find user from token in the database
         guard let user = try await User.find(payload.id, on: req.db) else {
-            throw Abort(.notFound)
+            throw Abort(.notFound, reason: "User not found")
         }
+
+        // Get user ID
         guard let userUUID = user.id else {
             throw Abort(.badRequest, reason: "Invalid user ID format")
         }
 
+        // Finds all transactions with this user ID in database
         let transactions = try await Transaction.query(on: req.db)
             .filter(\.$user.$id == userUUID)
             .all()
-
         if transactions.isEmpty {
             throw Abort(.notFound, reason: "No transaction found for this user")
         }
 
+        // Returns the transactions in an array of transaction DTOs
         return transactions.map{ $0.toTransactionDTO() }
     }
 }
